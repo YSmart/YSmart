e9b635b51e29c3bd3375a2ac046c12fbd8be7b09
Yuan Yuan <yuanyuan0526@gmail.com>
2012-01-10 22:08:58 +0000

    license
    git-svn-id: http://ysmart.googlecode.com/svn/trunk@221 1d6e5d1b-7b3d-7f63-4173-3946eeb354c0

--- XML2MapReduce/config.py
+++ XML2MapReduce/config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 """
+   Copyright (c) 2012 The Ohio State University.
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
