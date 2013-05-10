20f3985d7326abd9600d2806b779ee8480a4662d
meisam.fathi@gmail.com <meisam.fathi@gmail.com@1d6e5d1b-7b3d-7f63-4173-3946eeb354c0>
2013-05-10 23:25:04 +0000

    Adding the expected XML files for each query.
    These files are generated with the older version of YSmart (The version that uses ANTLR/C). I'm adding these files to make sure the ANTLR/python version of YSmart generates exactly the same output.
    
    git-svn-id: http://ysmart.googlecode.com/svn/branches/pure-python@236 1d6e5d1b-7b3d-7f63-4173-3946eeb354c0

--- test/testysmartparser.py
+++ test/testysmartparser.py
@@ -3,13 +3,14 @@ Created on May 7, 2013
 
 @author: fathi
 '''
-import unittest
-import antlr3
-from YSmartPythonishLexer import YSmartPythonishLexer
-from YSmartPythonishLexer import *  # import all the tokens 
-
+from YSmartPythonishLexer import *  # import all the tokens
+from YSmartPythonishParser import *
 from antlr3.tokens import CommonToken
 
+import antlr3
+import sql2xml
+import unittest
+from sql2xml import toXml
 
 class Test(unittest.TestCase):
 
@@ -50,30 +51,83 @@ class Test(unittest.TestCase):
 
         pass
 
-    def testSbbQuery1_1Lexer(self):
-        with open("ssb_test/q1_1.sql") as f:
-            query = f.read().upper()
-            cStream = antlr3.StringStream(query)
-            lexer = YSmartPythonishLexer(cStream)
-            for t in lexer:
-                print "Token %s" % t.text
-                
-            pass
+    def testTpch1Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/1.sql") 
 
-    def testTpch1Lexer(self):
-        with open("tpch_test/1.sql") as f:
-            query = f.read().upper()
-            cStream = antlr3.StringStream(query)
-            lexer = YSmartPythonishLexer(cStream)
-            for t in lexer:
-                print "Token %s" % t.text
-                
-            pass
-        self.fail("Lets fail")
+    def testTpch10Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/10.sql") 
 
-    def testBasicParser(self):
-        pass
+    def testTpch17Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/17.sql") 
+
+    def testTpch18Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/18.sql") 
+
+    def testTpch21Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/21.sql") 
+
+    def testTpch3Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/3.sql") 
+
+    def testTpch5Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/5.sql") 
+
+    def testTpch6Parser(self):
+        self.sql2XmlTestScaffold("tpch_test/6.sql") 
+
+    def testSSb1_1Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q1_1.sql") 
+
+    def testSSb1_2Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q1_2.sql") 
+
+    def testSSb1_3Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q1_3.sql") 
+
+    def testSSb2_1Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q2_1.sql") 
+
+    def testSSb2_2Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q2_2.sql") 
+
+    def testSSb2_3Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q2_3.sql") 
+
+    def testSSb3_1Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q3_1.sql") 
+
+    def testSSb3_2Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q3_2.sql") 
+
+    def testSSb3_3Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q3_3.sql") 
+
+    def testSSb4_4Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q3_4.sql") 
+
+    def testSSb4_1Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q4_1.sql") 
+
+    def testSSb4_2Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q4_2.sql") 
+
+    def testSSb4_3Parser(self):
+        self.sql2XmlTestScaffold("ssb_test/q4_3.sql") 
+
+
+
+    def sql2XmlTestScaffold(self, inputFileName):
         
+        errorMsg = """Expected output and produced output do not match for %s:
+                         Expected output: %s
+                         ---------------------------------------------------------------
+                         Produced output: %s
+                         ==============================================================="""
+        producedXml = toXml(open(inputFileName)).upper()
+        
+        expectedXml = open(inputFileName + ".xml").read().upper()
+        self.assertEqual(expectedXml, producedXml, errorMsg % (inputFileName, expectedXml, producedXml))
+
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testParser']
     unittest.main()
