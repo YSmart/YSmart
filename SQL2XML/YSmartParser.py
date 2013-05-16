de059e58c9f8d37d82f0a701204a768c1e8efb1b
meisam.fathi@gmail.com <meisam.fathi@gmail.com@1d6e5d1b-7b3d-7f63-4173-3946eeb354c0>
2013-05-16 18:33:36 +0000

    Adding and updating the boilerplate license code Also updaing the quic_start and system_requirements file
    git-svn-id: http://ysmart.googlecode.com/svn/branches/pure-python@250 1d6e5d1b-7b3d-7f63-4173-3946eeb354c0

--- SQL2XML/YSmartParser.py
+++ SQL2XML/YSmartParser.py
@@ -1,5 +1,23 @@
 # $ANTLR 3.3 Nov 30, 2010 12:45:30 YSmart.g 2013-05-04 18:21:05
 
+"""
+   Copyright (c) 2013 The Ohio State University.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+"""
+
+
 from antlr3 import *
 from antlr3.compat import set, frozenset
 from antlr3.tree import *
